4b1b1de27d89443a757d73143911f88b33f0d7a6
Not-Donno <abhinavgautam123456@gmail.com>
2025-01-11 16:32:19 +0545

    login

--- login.js
+++ login.js
@@ -1,11 +1,25 @@
 const container = document.getElementById("container");
 const registerbtn = document.getElementById("register");
 const loginbtn = document.getElementById("login");
-
 registerbtn.addEventListener("click", () => {
   container.classList.add("active");
 });
 
 loginbtn.addEventListener("click", () => {
   container.classList.remove("active");
+});
+
+// Handle login
+const signInForm = document.querySelector(".sign-in form");
+signInForm.addEventListener("submit", (event) => {
+  event.preventDefault(); 
+  const inputs = signInForm.querySelectorAll("input");
+  const username = inputs[0].value;
+  const password = inputs[1].value;
+
+  if (username === "admin" && password === "admin") {
+    window.location.href = "HomePage.html";
+  } else {
+    alert("Invalid username or password!");
+  }
 });
